342174e81c55ac7ac071844c3c25aa79d3fa9e6d
KarmezZ <karol.samsel.infoinf@gmail.com>
2024-07-13 02:52:08 +0200

    fixed static serving

--- school_website/settings.py
+++ school_website/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i2prd1ifcwd-=8!$!yq$oh39f8$ari5(a*)kd2kw-aa*wab=7w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -46,6 +46,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'school_website.urls'
